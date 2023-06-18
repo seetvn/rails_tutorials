62220a43e2a2780231fa7dd9c2506c2639ec62d4
Steven Wai Yan <waiyansteven2002@gmail.com>
2023-06-18 01:19:45 +0100

    generated Article model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  
 
   get "/articles", to: "articles#index"
   # The priority is based upon order of creation: first created -> highest priority.
