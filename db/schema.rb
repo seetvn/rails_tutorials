36f28d93d2da04380953af9b4d0ce28677d89e35
Steven Wai Yan <waiyansteven2002@gmail.com>
2023-06-18 01:28:43 +0100

    created table in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230616120707) do
+ActiveRecord::Schema.define(version: 20230618001856) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
