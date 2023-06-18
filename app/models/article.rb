81c42b9420bbc2cc7f2902e1eb049932a7b5ec17
Steven Wai Yan <waiyansteven2002@gmail.com>
2023-06-18 18:47:37 +0100

    added validation for model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
     def change
         create_table :articles do |t|
           t.string :title
