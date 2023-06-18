49e70812b58a3c10c3dbf8b2b4a4e071a16a09cf
Steven Wai Yan <waiyansteven2002@gmail.com>
2023-06-18 15:18:03 +0100

    created new routes RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "articles#index"
 
+  resources :articles
+  
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
