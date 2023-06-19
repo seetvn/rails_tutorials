c432ec5a6ad7fe2591ffa5f0ca0fd7ab4d5aa6bf
Steven Wai Yan <waiyansteven2002@gmail.com>
2023-06-19 12:06:52 +0100

    deleted repeated unnecessary table code

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,12 +2,5 @@ class Article < ActiveRecord::Base
     has_many :comments
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
-    def change
-        create_table :articles do |t|
-          t.string :title
-          t.text :body
-    
-          t.timestamps
-        end
-      end
+ 
 end
