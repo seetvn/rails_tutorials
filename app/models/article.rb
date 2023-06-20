998f03277e060b1fd6c7a487b39db66ca45f8216
Steven Wai Yan <waiyansteven2002@gmail.com>
2023-06-20 09:43:36 +0100

    commented out status for controllers and added it for models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,15 @@
 class Article < ActiveRecord::Base
     has_many :comments
+
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
-   
+    VALID_STATUSES = ['public', 'private', 'archived']
+
+    validates :status, inclusion: { in: VALID_STATUSES }
+
+    def archived?
+        status == 'archived'
+    end
   end
  
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
-  
+  # VALID_STATUSES = ['public', 'private', 'archived']
 
+  # validates :status, inclusion: { in: VALID_STATUSES }
 
-
- 
+  # def archived?
+  #   status == 'archived'
+  # end
 end
+  
 
